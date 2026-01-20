2c47b0f0ab3dda133ba667e58e34bb8b1b9a91cc
Lalit-mahata <lm3654725@gmail.com>
2026-01-20 22:22:08 +0545

    controller

--- resources/views/admin/product_attribute/manage.blade.php
+++ resources/views/admin/product_attribute/manage.blade.php
@@ -1,4 +1,7 @@
 @extends('admin.layouts.layout')
+@section('admin_page_title')
+Product Attribute Manage
+@endsection
 @section('admin_layout')
 <h3>Product Attribute Manage</h3>
 
--- resources/views/admin/sub_category/create.blade.php
+++ resources/views/admin/sub_category/create.blade.php
@@ -1,4 +1,7 @@
 @extends('admin.layouts.layout')
+@section('admin_page_title')
+Sub Category Create
+@endsection
 @section('admin_layout')
 <h3>Sub Category Create</h3>
 
