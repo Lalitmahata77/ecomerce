277714af11cbb8472484137199c1bf6023b15dd7
Lalit-mahata <lm3654725@gmail.com>
2026-01-20 22:54:26 +0545

    sidebar route

--- app/Http/Controllers/Admin/ProductAttributeController.php
+++ app/Http/Controllers/Admin/ProductAttributeController.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 class ProductAttributeController extends Controller
 {
     public function index(){
-        return view('admin.product_attribute.create');
+        return view('admin.productattribute.create');
     }
 
     public function manage(){
-        return view('admin.product_attribute.manage');
+        return view('admin.productattribute.manage');
     }
 }
--- app/Http/Controllers/Admin/ProductDiscountController.php
+++ app/Http/Controllers/Admin/ProductDiscountController.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 class ProductDiscountController extends Controller
 {
     public function index(){
-        return view('admin.product_discount.create');
+        return view('admin.discount.create');
     }
 
     public function manage(){
-        return view('admin.product_discount.manage');
+        return view('admin.discount.manage');
     }
 }
